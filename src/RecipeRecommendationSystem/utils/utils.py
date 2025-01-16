bcfdaf16f31505516572ce4e0f15391e96ef74c9
sreelakshmisuresh10 <lakshmipillai1010@gmail.com>
2025-01-16 23:48:44 +0530

    aws to local

--- src/RecipeRecommendationSystem/utils/utils.py
+++ src/RecipeRecommendationSystem/utils/utils.py
@@ -4,28 +4,39 @@ import pandas as pd
 from io import BytesIO
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 import os
-
-s3_client = boto3.client(
-    "s3",
-    region_name=os.getenv("AWS_REGION"),
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-)
-# Initialize S3 client
-s3_client = boto3.client("s3")
-BUCKET_NAME = "recipe-recommender-data"
+from joblib import dump, load
+s3_enabled = False
+s3_client = None
+if s3_enabled:
+    s3_client = boto3.client(
+        "s3",
+        region_name=os.getenv("AWS_REGION"),
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    )
+    # Initialize S3 client
+    s3_client = boto3.client("s3")
+    BUCKET_NAME = "recipe-recommender-data"
 
 
 def save_model(model, model_name):
-    model_bytes = BytesIO()
-    joblib.dump(model, model_bytes)
-    model_bytes.seek(0)
+    
 
     try:
-        s3_client.upload_fileobj(
-            model_bytes, BUCKET_NAME, f"models/{model_name}.joblib"
-        )
-        print(f"Model {model_name} saved to S3 bucket {BUCKET_NAME} in 'models' folder")
+        if s3_enabled:
+            model_bytes = BytesIO()
+            joblib.dump(model, model_bytes)
+            model_bytes.seek(0)
+            
+            s3_client.upload_fileobj(
+                model_bytes, BUCKET_NAME, f"models/{model_name}.joblib"
+            )
+            print(f"Model {model_name} saved to S3 bucket {BUCKET_NAME} in 'models' folder")
+        else:
+            print("sneh pillai")
+            dump(model, f"data/models/{model_name}.joblib")
+            print(f"Model {model_name} saved to local fs in 'data/models' directory")
+            
     except (NoCredentialsError, PartialCredentialsError) as e:
         print(f"Credentials error: {e}")
     except Exception as e:
@@ -35,17 +46,21 @@ def save_model(model, model_name):
 def load_model(model_name):
     try:
         # Download the model from S3 to a BytesIO object
-        model_bytes = BytesIO()
-        s3_client.download_fileobj(
-            BUCKET_NAME, f"models/{model_name}.joblib", model_bytes
-        )
-        model_bytes.seek(0)
-
-        # Load the model
-        model = joblib.load(model_bytes)
-        print(
-            f"Model {model_name} loaded from S3 bucket {BUCKET_NAME} in 'models' folder"
-        )
+        
+        if s3_enabled:
+            model_bytes = BytesIO()
+            s3_client.download_fileobj(
+                BUCKET_NAME, f"models/{model_name}.joblib", model_bytes
+            )
+            model_bytes.seek(0)
+            # Load the model
+            model = joblib.load(model_bytes)
+            print(
+                f"Model {model_name} loaded from S3 bucket {BUCKET_NAME} in 'models' folder"
+            )
+        else:
+            model = load(f"data/models/{model_name}.joblib")
+       
         return model
     except (NoCredentialsError, PartialCredentialsError) as e:
         print(f"Credentials error: {e}")
@@ -62,8 +77,12 @@ def save_dataframe(df, filename):
 
     # Upload to S3
     try:
-        s3_client.upload_fileobj(csv_bytes, BUCKET_NAME, f"data/processed/{filename}")
-        print(f"DataFrame saved to S3 bucket {BUCKET_NAME} in 'data/processed' folder")
+        if s3_enabled:
+            s3_client.upload_fileobj(csv_bytes, BUCKET_NAME, f"data/processed/{filename}")
+            print(f"DataFrame saved to S3 bucket {BUCKET_NAME} in 'data/processed' folder")
+        else:
+            df.to_csv(f"data/processed/{filename}", index=False)
+            print(f"DataFrame saved to local fs in 'data/processed' dir")
     except (NoCredentialsError, PartialCredentialsError) as e:
         print(f"Credentials error: {e}")
     except Exception as e:
@@ -74,14 +93,17 @@ def load_dataframe(filename):
     try:
         # Download the CSV from S3 to a BytesIO object
         csv_bytes = BytesIO()
-        s3_client.download_fileobj(BUCKET_NAME, f"data/processed/{filename}", csv_bytes)
-        csv_bytes.seek(0)
+        if s3_enabled:
+            s3_client.download_fileobj(BUCKET_NAME, f"data/processed/{filename}", csv_bytes)
+            csv_bytes.seek(0)
 
-        # Load the DataFrame
-        df = pd.read_csv(csv_bytes, encoding="utf-8")
-        print(
-            f"DataFrame loaded from S3 bucket {BUCKET_NAME} in 'data/processed' folder"
-        )
+            # Load the DataFrame
+            df = pd.read_csv(csv_bytes, encoding="utf-8")
+            print(
+                f"DataFrame loaded from S3 bucket {BUCKET_NAME} in 'data/processed' folder"
+            )
+        else:
+            df = pd.read_csv(f"data/processed/{filename}")
         return df
     except (NoCredentialsError, PartialCredentialsError) as e:
         print(f"Credentials error: {e}")
